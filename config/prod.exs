529bfdb11831ab828dd33e3d83a8b18405878d9a
Nick Beukema <nick.beukema@gmail.com>
2018-03-21 05:18:34 -0400

    Add missing comma

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :app, App.Endpoint,
   http: [port: {:system, "PORT"}],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
-  url: [host: "todos-phoenix-backend.herokuapp.com/ ", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "todos-phoenix-backend.herokuapp.com", port: 443],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
 
