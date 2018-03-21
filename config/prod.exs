7d951720fdd9e9d445cded73a3020d870df2386a
Nick Beukema <nick.beukema@gmail.com>
2018-03-21 05:16:09 -0400

    Ready for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :app, App.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  url: [host: "todos-phoenix-backend.herokuapp.com/ ", port: 443],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
 
