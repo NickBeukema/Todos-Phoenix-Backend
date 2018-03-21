41f77b613966785e35eadd98d24886fe82f24e6d
Nick Beukema <nick.beukema@gmail.com>
2018-03-21 05:59:19 -0400

    Configure travis

--- config/prod.exs
+++ config/prod.exs
@@ -15,13 +15,7 @@ config :app, App.Endpoint,
   http: [port: {:system, "PORT"}],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "todos-phoenix-backend.herokuapp.com", port: 443],
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
-
-config :app, App.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  pool_size: 20
 
 # Do not print debug messages in production
 config :logger, level: :info
